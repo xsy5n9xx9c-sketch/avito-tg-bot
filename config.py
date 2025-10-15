71c24e4b9be65a8b93c2c0324f46ae2c3cf17338
xsy5n9xx9c-sketch <xsy5n9xx9c@privaterelay.appleid.com>
2025-10-15 14:59:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 TELEGRAM_BOT_TOKEN = "8345040978:AAGZ-eVfbxeAiHr2AJhoRqVcyaXOii6sk2Q" 
-WEBHOOK_HOST = "https://YOUR_RENDER_URL"        # URL Render после деплоя
+WEBHOOK_HOST = "https://avito-tg-bot-4dno.onrender.com"        # URL Render после деплоя
 DB_FILE = "database.db"
